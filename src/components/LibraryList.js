85f4a99ce149f44fedc63928be77233c6d459431
plasx <plasx@live.com>
2016-12-17 21:48:30 -0500

    redux is some hell...

---
+++ src/components/LibraryList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+  render() {
+    console.log(this.props);
+    return;
+  }
+}
+
+const mapStateToProps = state => {
+  console.log(state);
+};
+
+export default connect(mapStateToProps)(LibraryList);
